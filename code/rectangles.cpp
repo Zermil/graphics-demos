800ae894735f888b26586263de703eceed9aab01
Aiden <marcin.madry@interia.com>
2023-07-26 16:27:29 +0200

    Readability of rectangles.cpp demo

--- code/rectangles.cpp
+++ code/rectangles.cpp
@@ -12,14 +12,19 @@ internal inline int vec2_dot(Vec2 *a, Vec2 *b)
     return(a->x*b->x + a->y*b->y);
 }
 
-internal inline void vec2_rotate_around(Vec2 *a, Vec2 *center)
+// @Note: This is just the basic formula for point/vector rotation.
+internal void vec2_rotate_many_points_around(Vec2 *points, size_t length, Vec2 *centroid)
 {
+    ERROR_EXIT(points == NULL, "[ERROR]: Array of points was NULL\n");
+    
     const float sine = sinf(g_angle);
     const float cosine = cosf(g_angle);
-    Vec2 rot = { a->x - center->x, a->y - center->y };
     
-    a->x = (int) (center->x + rot.x*cosine - rot.y*sine);
-    a->y = (int) (center->y + rot.x*sine + rot.y*cosine);
+    for (size_t i = 0; i < length; ++i) {
+        Vec2 rot = { points[i].x - centroid->x, points[i].y - centroid->y };
+        points[i].x = (int) (centroid->x + rot.x*cosine - rot.y*sine);
+        points[i].y = (int) (centroid->y + rot.x*sine + rot.y*cosine);
+    }
 }
 
 // @Note: 'Technically' :nerd_emoji: this is not a _correct_
@@ -58,10 +63,6 @@ internal void render_rectangle_quad(int x0, int y0,
                                     int x3, int y3,
                                     uint32_t colour)
 {
-    // @Note: Finding the centroid of a quad, this will help
-    // us determine whether or not it actually is a rectangle.
-    Vec2 centroid = { (x0 + x1 + x2 + x3)/4, (y0 + y1 + y2 + y3)/4 };
-
     // @Note: If we wanted to we can sort points clockwise somewhere here
     // and check that provided points actually form a rectangle. (ie. sorting by
     // their angle from the center)
@@ -75,6 +76,12 @@ internal void render_rectangle_quad(int x0, int y0,
     int y_max = MAX(MAX(y0, y1), MAX(y2, y3));
     int y_min = MIN(MIN(y0, y1), MIN(y2, y3));
 
+    Vec2 AB = { x1 - x0, y1 - y0 };
+    int dot_ab = vec2_dot(&AB, &AB);
+    
+    Vec2 BC = { x2 - x1, y2 - y1 };
+    int dot_bc = vec2_dot(&BC, &BC);
+    
     for (int y = y_min; y <= y_max; ++y) {
         for (int x = x_min; x <= x_max; ++x) {
             // Note: Boundary check.
@@ -82,16 +89,12 @@ internal void render_rectangle_quad(int x0, int y0,
 
             // @Note: Check if point is in rectangle.
             Vec2 AP = { x - x0, y - y0 };
-            Vec2 AB = { x1 - x0, y1 - y0 };
-            Vec2 BC = { x2 - x1, y2 - y1 };
 
             int dot_ap = vec2_dot(&AB, &AP);
-            int dot_ab = vec2_dot(&AB, &AB);
             int dot_bp = vec2_dot(&BC, &AP);
-            int dot_bc = vec2_dot(&BC, &BC);
             
-            if ((dot_ap >= 0.0f && dot_ap <= dot_ab) &&
-                (dot_bp >= 0.0f && dot_bp <= dot_bc))
+            if ((dot_ap >= 0 && dot_ap <= dot_ab) &&
+                (dot_bp >= 0 && dot_bp <= dot_bc))
             {
                 ARRAY_AT(frame_buffer, x, y) = colour;
             }
@@ -101,20 +104,23 @@ internal void render_rectangle_quad(int x0, int y0,
 
 internal void render(float dt)
 {
-    g_angle += TURNS(0.35f)*dt;
+    g_angle += TURNS(0.30f)*dt;
     
     // @Note: Simple rectangle.
     {
         uint32_t colours[] = { 0x000000FF, 0xA3A3A3FF, 0xFFFFFFFF, 0x800080FF};
         int w = 200;
         int h = 30;
-        int x0 = WIDTH/2 - w/2;
-        int y0 = 10;
+        
+        Vec2 p0 = {
+            WIDTH/2 - w/2,
+            10
+        };
 
         float t = (sinf(g_angle) + 1)/4 + 0.5f;
     
         for (int i = 0; i < ARRAY_SIZE(colours); ++i) {
-            render_simple_rectangle(x0, y0 + i*h, w, h, interpolate_colour(colours[i], t));
+            render_simple_rectangle(p0.x, p0.y + i*h, w, h, interpolate_colour(colours[i], t));
         }
     }
 
@@ -123,17 +129,19 @@ internal void render(float dt)
         int w = 100;
         int h = 150;
         
-        Vec2 p0 = { WIDTH/2 - w/2, HEIGHT/2 };
-        Vec2 p1 = { WIDTH/2 + w/2, HEIGHT/2 };
-        Vec2 p2 = { WIDTH/2 + w/2, HEIGHT/2 + h };
-        Vec2 p3 = { WIDTH/2 - w/2, HEIGHT/2 + h };
-        Vec2 centroid = { (p0.x + p1.x + p2.x + p3.x)/4, (p0.y + p1.y + p2.y + p3.y)/4 };
+        Vec2 p[] = {
+            { WIDTH/2 - w/2, HEIGHT/2 },
+            { WIDTH/2 + w/2, HEIGHT/2 },
+            { WIDTH/2 + w/2, HEIGHT/2 + h },
+            { WIDTH/2 - w/2, HEIGHT/2 + h },
+        };
         
-        vec2_rotate_around(&p0, &centroid);
-        vec2_rotate_around(&p1, &centroid);
-        vec2_rotate_around(&p2, &centroid);
-        vec2_rotate_around(&p3, &centroid);
+        Vec2 centroid = {
+            (p[0].x + p[1].x + p[2].x + p[3].x)/4,
+            (p[0].y + p[1].y + p[2].y + p[3].y)/4
+        };
         
-        render_rectangle_quad(p0.x, p0.y, p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, 0x5BCEFAFF);
+        vec2_rotate_many_points_around(p, ARRAY_SIZE(p), &centroid);
+        render_rectangle_quad(p[0].x, p[0].y, p[1].x, p[1].y, p[2].x, p[2].y, p[3].x, p[3].y, 0x5BCEFAFF);
     }
 }
