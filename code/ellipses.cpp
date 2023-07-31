3c9f235611fb5656d29974cfc0420def6b274e3d
Aiden <marcin.madry@interia.com>
2023-07-31 18:10:02 +0200

    Finish ellipses demo

--- code/ellipses.cpp
+++ code/ellipses.cpp
@@ -1,8 +1,23 @@
 #include "../main.cpp"
 
+#define MAX_SQUISH 150
+#define MAX_SQUASH 100
+#define MIN_SQUISH 30
+#define MIN_SQUASH 20
+#define ANGLE_SPEED 0.50f
+
+global float g_angle = 0.0f;
+
+// @Note: Linearly interpolate between two values and provided percent
+// (lerpf my beloved)
+internal inline float lerpf(float a, float b, float t)
+{
+    return(a*(1.0f - t) + b*t);
+}
+
 // @Note: There is an algorithm for midpoint ellipse, but I feel
 // like this gets the points across and is easier to understand at a glance.
-internal void render_draw_ellipse(int cx, int cy, int rx, int ry)
+internal void render_draw_ellipse(int cx, int cy, int rx, int ry, uint32_t colour)
 {
     for (int y = cy - ry; y <= cy + ry; ++y) {
         if (y < 0 || y >= HEIGHT) continue;
@@ -17,7 +32,7 @@ internal void render_draw_ellipse(int cx, int cy, int rx, int ry)
             float ycheck = (float) (dy*dy) / (float) (ry*ry);
             
             if (xcheck + ycheck <= 1.0f) {
-                ARRAY_AT(frame_buffer, x, y) = 0xFF0000FF;
+                ARRAY_AT(frame_buffer, x, y) = colour;
             }
 
             // @Note: You could argue that we can just do something like this
@@ -39,7 +54,15 @@ internal void render_draw_ellipse(int cx, int cy, int rx, int ry)
 
 internal void render(float dt)
 {
-    UNUSED(dt);
+    g_angle += TURNS(ANGLE_SPEED)*dt;
+    
+    // @Note: Sin value between 0..1
+    float sine = (sinf(g_angle) + 1)/2;
 
-    render_draw_ellipse(WIDTH/2, HEIGHT/2, 600, 120);
+    int rx = (int) (MAX_SQUISH*(1.0f - sine)) + MIN_SQUISH;
+    int ry = (int) (MAX_SQUASH*sine) + MIN_SQUASH;
+    int cx = WIDTH/2;
+    int cy = HEIGHT - (int) lerpf((float) ry, HEIGHT/2, sine);
+    
+    render_draw_ellipse(cx, cy, rx, ry, 0xEF7627FF);
 }
