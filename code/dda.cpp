2b09aae47f5fc12e4ceb62965a3358e4a07690ac
Aiden <marcin.madry@interia.com>
2024-08-16 18:54:59 +0200

    Add useful DDA line drawing algorithm

---
+++ code/dda.cpp
@@ -0,0 +1,41 @@
+#include "../main.cpp"
+
+struct Vec2f {
+    float x;
+    float y;
+};
+
+internal void render_line(Vec2f A, Vec2f B, unsigned int col)
+{
+    Vec2f dir = {
+        B.x - A.x,
+        B.y - A.y
+    };
+    
+    Vec2f start = {
+        floorf(A.x),
+        floorf(A.y)
+    };
+    
+    float steps = fabsf(dir.x) > fabsf(dir.y) ? fabsf(dir.x) : fabsf(dir.y);
+    Vec2f ray_step = {
+        dir.x / steps,
+        dir.y / steps
+    };
+
+    for (int i = 0; i <= (int) steps; ++i) {
+        ARRAY_AT(frame_buffer, (int) start.x, (int) start.y) = col;
+        start.x += ray_step.x;
+        start.y += ray_step.y;
+    }    
+}
+
+internal void render(float dt)
+{
+    UNUSED(dt);
+    
+    Vec2f A = { 0.0f, 0.0f };
+    Vec2f B = { WIDTH*.5f, HEIGHT*.5f };
+    
+    render_line(A, B, 0xFF0000FF);
+}
